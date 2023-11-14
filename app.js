7415de82a31488e5da365afd7dd6b6982a421809
Abdulaziz-developer-info <abdulazizdeveloperinfo@gmail.com>
2023-11-15 01:10:30 +0500

    Malumot turlari

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// O'zgaruvchilar
+
+// bu let o'zgaruvchilari hisoblanadi
+let ism = "Abdulaziz"
+let yoshi = 20
+
+// O'zgarmaslar 
+const const_text = "Hi text"
+
+// O'zgaruvchilar 
+var var_text = "Hi text"
+// ----------------------------------------
+
+
+// Malumot turlari
+
+// Bu number type 
+// Number => 1 2 3 -5 0.1
+
+// Bu string type 
+// String => "Hi text",'Hi text'
+
+// Bu boolean type hisoblanadi bu ikkixil parametr qabul qiladi true yoki false
+// Boolean => true | false
+
+// Bu Null type yani umuman malumot yuq Matimatik jihatdan 0 ga teng 
+// Null => ma'lumot yuq 
+
+
+// Bu undefined O'zgaruvchi bor lekin qiymati yuq
+// Undefined 
+
+// Bu object malumotlar arraylar ko'p malumotlar
+// Object
+
+// Bu symbol bu objectlar bilan ishlatilinadi
+// Symbol 
+
